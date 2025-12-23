5f0c65dac42567cf92f2589a196d390b05814cc9
peknight <JKpeknight@gmail.com>
2025-12-23 13:10:15 +0800

    update

--- auth-core/shared/src/main/scala/com/peknight/auth/token/Token.scala
+++ auth-core/shared/src/main/scala/com/peknight/auth/token/Token.scala
@@ -1,6 +1,8 @@
 package com.peknight.auth.token
 
-sealed trait Token
+sealed trait Token:
+  def token: String
+end Token
 object Token:
   case class Bearer(token: String) extends Token
 end Token
