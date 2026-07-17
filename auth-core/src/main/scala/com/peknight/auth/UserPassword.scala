076b52a2a7030d72d3d7d3b61a0f070630953db1
peknight <JKpeknight@gmail.com>
2026-07-17 17:25:38 +0800

    update

---
+++ auth-core/src/main/scala/com/peknight/auth/UserPassword.scala
@@ -0,0 +1,16 @@
+package com.peknight.auth
+
+import cats.{Monad, Show}
+import com.peknight.codec.config.given
+import com.peknight.codec.cursor.Cursor
+import com.peknight.codec.reader.{Key, Reader}
+import com.peknight.codec.sum.{NullType, ObjectType, StringType}
+import com.peknight.codec.{Codec, Decoder}
+
+case class UserPassword(user: User, password: Password)
+object UserPassword:
+  given codecUserPassword[F[_]: Monad, S: {ObjectType, NullType, StringType, Show}]: Codec[F, S, Cursor[S], UserPassword] =
+    Codec.derived[F, S, UserPassword]
+  given keyDecodeUserPassword[F[_]](using Monad[F], Reader[F, String]): Decoder[F, Key, UserPassword] =
+    Decoder.derivedByKey[F, UserPassword]
+end UserPassword
