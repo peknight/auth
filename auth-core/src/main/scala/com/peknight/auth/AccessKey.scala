515105accbff6c02c1daf08bddc53da78f2115ea
peknight <JKpeknight@gmail.com>
2026-07-17 14:39:59 +0800

    update

--- auth-core/src/main/scala/com/peknight/auth/AccessKey.scala
+++ auth-core/src/main/scala/com/peknight/auth/AccessKey.scala
@@ -1,11 +1,9 @@
 package com.peknight.auth
 
-import cats.effect.std.Env
-import cats.{Monad, MonadError, Show}
+import cats.{Monad, Show}
 import com.peknight.codec.config.given
 import com.peknight.codec.cursor.Cursor
-import com.peknight.codec.effect.instances.envReader.given
-import com.peknight.codec.reader.Key
+import com.peknight.codec.reader.{Key, Reader}
 import com.peknight.codec.sum.{NullType, ObjectType, StringType}
 import com.peknight.codec.{Codec, Decoder}
 
@@ -13,6 +11,6 @@ case class AccessKey(id: Identifier, secret: Secret)
 object AccessKey:
   given codecAccessKey[F[_]: Monad, S: {ObjectType, NullType, StringType, Show}]: Codec[F, S, Cursor[S], AccessKey] =
     Codec.derived[F, S, AccessKey]
-  given keyDecodeAccessKey[F[_]](using MonadError[F, Throwable], Env[F]): Decoder[F, Key, AccessKey] =
+  given keyDecodeAccessKey[F[_]](using Monad[F], Reader[F, String]): Decoder[F, Key, AccessKey] =
     Decoder.derivedByKey[F, AccessKey]
 end AccessKey
